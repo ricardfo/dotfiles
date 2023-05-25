b145e3b41ca5fa3c2e6eb9bc1dd1a94871371c54
Ricardo Fontoura <ricardfo@usp.br>
2023-05-25 16:59:47 -0300

    add config ignore git directory.

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,31 +1,36 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  keys = {
-    {"<leader>e",":NvimTreeToggle<CR>", mode = "n"},
-    {"<leader>n",":NvimTreeFindFileToggleCR>", mode = "n"},
-  },
-  version = "*",
-  init = function()
-    -- disable netrw
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    --
-  end,
-  config = function()
-    require("nvim-tree").setup {
-      sort_by = "case_sensitive",
-      view = {
-        width = 30,
-      },
-      renderer = {
-        group_empty = true,
-      },
-      filters = {
-        dotfiles = false,
-      },
-    }
-  end,
+  {"nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      {"<leader>e",":NvimTreeToggle<CR>", mode = "n"},
+      {"<leader>n",":NvimTreeFindFileToggleCR>", mode = "n"},
+    },
+    version = "*",
+    init = function()
+      -- disable netrw
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      --
+    end,
+    config = function()
+      require("nvim-tree").setup {
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+          custom = { "^.git$" },
+        },
+        git = {
+          ignore = false,
+        },
+      }
+    end,
+  }
 }
