ca9e9e2a90c779e58961555ce40d2f518520ce8c
Ricardo Fontoura <ricardfo@usp.br>
2024-06-21 09:27:37 -0300

    alter config nvim-tree
        O mapeamento do help do nvim-tree foi alterado de g? para ?

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -15,6 +15,21 @@ return {
       --
     end,
     config = function()
+
+      local function my_on_attach(bufnr)
+        local api = require "nvim-tree.api"
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- custom mappings
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+      end
+
       require("nvim-tree").setup {
         sort_by = "case_sensitive",
         view = {
